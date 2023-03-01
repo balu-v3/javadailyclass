e80f8a9890259ea3aeed3ebc0afefa5fc9e50e1f
balu-v3 <125967427+balu-v3@users.noreply.github.com>
2023-03-01 22:26:25 +0530

    Add files via upload

---
+++ multiarr.java
@@ -0,0 +1,23 @@
+class multiarr{
+
+	public static void main(String args[])
+{
+
+	int arr[][]={{1,4,3},{4,6,8},{9,11,13}};
+	
+
+
+
+
+	
+	for(int i=0;i<3;i++)
+	{
+	for(int j=0;j<3;j++)
+	{
+		System.out.println(arr[i][j]+"\t");
+	}
+
+	System.out.println("");
+	}
+}
+}
