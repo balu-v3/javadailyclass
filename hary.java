b9539b8ed80dac769258c28145d9563575da8346
balu-v3 <125967427+balu-v3@users.noreply.github.com>
2023-03-01 22:05:25 +0530

    Add files via upload

---
+++ hary.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class hary
+{
+
+	public static void main(String args[])
+
+{
+		addition(1,4,3,9);
+}
+	public static void addition(int a,int b)
+	{
+		System.out.println("first addition "+(a+b));
+	}
+	public static void addition(int a,int b,int c)
+	{
+		System.out.println("second addition "+(a+b+c));
+	}
+	public static void addition(int a,int b,int c,int d)
+	{
+		System.out.println("third addition "+(a+b+c+d));
+	}
+}
